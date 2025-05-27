30f23dec8002233463c89139931c3f01c5d239f9
Wallace Martins <wmvwallace@gmail.com>
2025-05-27 17:19:59 -0300

    feat: Adicionar controlador de embalagem e remover previsão do tempo
    Implementado o `EmbalagemController.cs` para processar embalagens via API, com um método `Post` que lida com entradas e retorna resultados ou erros. Removido o `WeatherForecastController.cs`, que não é mais necessário.

---
+++ Controllers/EmbalagemController.cs
@@ -0,0 +1,32 @@
+﻿using LojaManoel.Models;
+using LojaManoel.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LojaManoel.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EmbalagemController : ControllerBase
+    {
+        private readonly IEmbalagemService _embalagemService;
+
+        public EmbalagemController(IEmbalagemService embalagemService)
+        {
+            _embalagemService = embalagemService;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<EmbalagemOutput>> Post([FromBody] EmbalagemInput input)
+        {
+            try
+            {
+                var result = await _embalagemService.ProcessarEmbalagem(input);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Erro ao processar a embalagem: {ex.Message}");
+            }
+        }
+    }
+}
