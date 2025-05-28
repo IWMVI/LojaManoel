ff34b0925cea5a1ec92fc825afcccad129343c38
Wallace Martins <wmvwallace@gmail.com>
2025-05-27 23:46:07 -0300

    Adiciona migração inicial para o banco de dados
    Implementa as entidades `Caixa`, `CaixaSelecionada`, `Pedido` e `Produto`, com suas respectivas colunas e tipos de dados. Insere dados iniciais na tabela `Caixas` e configura as relações entre as entidades, incluindo chaves primárias e estrangeiras. Código gerado automaticamente pelo Entity Framework Core com anotações de versão e comprimento máximo do identificador.

---
+++ Data/Migrations/20250528024207_InitialMigration.cs
@@ -0,0 +1,146 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace LojaManoel.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Caixas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Altura = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Largura = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Comprimento = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Caixas", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Pedidos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pedidos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CaixasSelecionadas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PedidoId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CaixasSelecionadas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CaixasSelecionadas_Pedidos_PedidoId",
+                        column: x => x.PedidoId,
+                        principalTable: "Pedidos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Produtos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Altura = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Largura = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Comprimento = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    PedidoId = table.Column<int>(type: "int", nullable: false),
+                    CaixaId = table.Column<int>(type: "int", nullable: true),
+                    Caixa = table.Column<int>(type: "int", nullable: true),
+                    CaixaSelecionadaId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Produtos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Produtos_CaixasSelecionadas_CaixaSelecionadaId",
+                        column: x => x.CaixaSelecionadaId,
+                        principalTable: "CaixasSelecionadas",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Produtos_Caixas_CaixaId",
+                        column: x => x.CaixaId,
+                        principalTable: "Caixas",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Produtos_Pedidos_PedidoId",
+                        column: x => x.PedidoId,
+                        principalTable: "Pedidos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Caixas",
+                columns: new[] { "Id", "Altura", "Comprimento", "Largura", "Nome" },
+                values: new object[,]
+                {
+                    { 1, 30m, 80m, 40m, "Caixa 1" },
+                    { 2, 80m, 40m, 50m, "Caixa 2" },
+                    { 3, 50m, 60m, 80m, "Caixa 3" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CaixasSelecionadas_PedidoId",
+                table: "CaixasSelecionadas",
+                column: "PedidoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Produtos_CaixaId",
+                table: "Produtos",
+                column: "CaixaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Produtos_CaixaSelecionadaId",
+                table: "Produtos",
+                column: "CaixaSelecionadaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Produtos_PedidoId",
+                table: "Produtos",
+                column: "PedidoId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Produtos");
+
+            migrationBuilder.DropTable(
+                name: "CaixasSelecionadas");
+
+            migrationBuilder.DropTable(
+                name: "Caixas");
+
+            migrationBuilder.DropTable(
+                name: "Pedidos");
+        }
+    }
+}
